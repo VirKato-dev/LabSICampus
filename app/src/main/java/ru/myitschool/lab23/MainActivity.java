44b866622499310ea73939af431ab74f23350135
VirKato <dascat79@mail.ru>
2023-06-30 23:09:07 +0300

    init

--- app/src/main/java/ru/myitschool/lab23/MainActivity.java
+++ app/src/main/java/ru/myitschool/lab23/MainActivity.java
@@ -1,9 +1,13 @@
 package ru.myitschool.lab23;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import ru.myitschool.lab23.databinding.ActivityMainBinding;
 
@@ -14,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
 
+    /**
+     * Перенесён из отдельного файла-класса
+     */
+    public static class ShareFragment extends Fragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_share, container, false);
+        }
     }
 }
